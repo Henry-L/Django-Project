248257eb971bbd666bc403ce0602b557364f3ec3
unknown <henry.lee@TXWR341C5TQ.Taxware.local>
2014-02-10 16:04:30 -0500

    Modified Admin page; added detail, results, and vote pages.

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -67,7 +68,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -80,3 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Template Folder
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
