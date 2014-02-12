e531aa9a4d3a5f7d57193b8d85941148528fdedb
unknown <henry.lee@TXWR341C5TQ.Taxware.local>
2014-02-12 08:38:38 -0500

    Added HTML formatting to views

--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 
 from polls.models import Poll
+
 """
 def index(request):
     latest_poll_list = Poll.objects.order_by('-publication_date')[:5]
@@ -20,6 +21,7 @@ def index(request):
 
 def detail(request, poll_id):
     return HttpResponse("<br><font size='12' face='verdana'>You're looking at poll %s.</font>" % poll_id)
+
     """try:
         poll = Poll.objects.get(pk=poll_id)
     except Poll.DoesNotExist:
@@ -27,7 +29,7 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll': poll})"""
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    return HttpResponse("<br><font size='12' face='verdana'>You're looking at the results of poll %s.</font>" % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    return HttpResponse("<br><font size='12' face='verdana'>You're voting on poll %s.</font>" % poll_id)
