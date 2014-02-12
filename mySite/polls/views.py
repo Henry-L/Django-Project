c1b66357b13465d0995847745f88802e9aa40960
unknown <henry.lee@TXWR341C5TQ.Taxware.local>
2014-02-12 08:36:06 -0500

    Added additional views and URL paths.

--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Poll
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
 
+from polls.models import Poll
+"""
 def index(request):
     latest_poll_list = Poll.objects.order_by('-publication_date')[:5]
-    output = '<br>'.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+        })
+    return HttpResponse(template.render(context))
+    """
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-publication_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("<br><font size='12' face='verdana'>You're looking at poll %s.</font>" % poll_id)
+    """try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})"""
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
